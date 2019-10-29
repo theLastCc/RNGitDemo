96bf8942098bf641240264fd840104b3a35e19ff
xx <2846366510@qq.com>
2019-10-29 14:47:44 +0800

    createStackNavigator结束

--- page/homePage.js
+++ page/homePage.js
@@ -2,34 +2,34 @@ import React, {Component} from 'react';
 import {View, Text, Button} from 'react-native';
 
 export default class homePage extends Component {
-  //在这里定义每个页面的导航属性
-  static navigationOptions = {
-    title: 'Home',
-    headerBackTitle: '返回哈哈', //设置返回此页面的返回按钮文案，有长度限制
-  };
+    //在这里定义每个页面的导航属性
+    static navigationOptions = {
+        title: 'Home',
+        headerBackTitle: '返回哈哈', //设置返回此页面的返回按钮文案，有长度限制
+    };
 
-  render() {
-    const {navigation} = this.props;
-    return (
-      <View>
-        <Button
-          title={'page1'}
-          onPress={() => {
-            navigation.navigate('page1', {name: '动态的name1'});
-          }}></Button>
-        <Button
-          title={'page2'}
-          onPress={() => {
-            navigation.navigate('page2');
-          }}></Button>
-        <Button
-          title={'page3'}
-          onPress={() => {
-            navigation.navigate('page3', {});
-          }}></Button>
-        <Button title={'page4'}></Button>
-        <Button title={'page5'}></Button>
-      </View>
-    );
-  }
+    render() {
+        const {navigation} = this.props;
+        return (
+            <View>
+                <Button
+                    title={'page1'}
+                    onPress={() => {
+                        navigation.navigate('page1', {name: '动态的name1'});
+                    }}/>
+                <Button
+                    title={'page2'}
+                    onPress={() => {
+                        navigation.navigate('page2');
+                    }}/>
+                <Button
+                    title={'page3'}
+                    onPress={() => {
+                        navigation.navigate('page3', {});
+                    }}/>
+                <Button title={'page4'} onPress={}/>
+                <Button title={'page5'} onPress={}/>
+            </View>
+        );
+    }
 }
--- page/page3.js
+++ page/page3.js
@@ -1,34 +1,40 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput, StyleSheet} from 'react-native';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
 
 export default class page3 extends Component {
-  render() {
-    const {navigation} = this.props;
-    const {state, setParams} = navigation;
-    const {params} = state;
-    // debugger
-    const showText = params.mode === 'edit' ? '正在编辑' : '编辑完成';
-    return (
-      <View>
-        <Text style={styles.textStyle}>page3</Text>
-        <Text style={styles.showText}>{showText}</Text>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={text => {
-            setParams({title: text});
-          }}></TextInput>
-      </View>
-    );
-  }
+    render() {
+        const {navigation} = this.props;
+        const {state, setParams} = navigation;
+        const {params} = state;
+        // debugger
+        const showText = params.mode === 'edit' ? '正在编辑' : '编辑完成';
+        return (
+            <View>
+                <Text style={styles.textStyle}>page3</Text>
+                <Text style={styles.showText}>{showText}</Text>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={text => {
+                        setParams({title: text});
+                    }}/>
+                <Button
+                    title='go back'
+                    onPress={() => {
+                        navigation.goBack()
+                    }}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  textStyle: {
-    color: 'red',
-  },
-  textInput: {
-    height: 40,
-    borderWidth: 1,
-    marginTop: 10,
-  },
+    textStyle: {
+        color: 'red',
+    },
+    textInput: {
+        height: 40,
+        borderWidth: 1,
+        marginTop: 10,
+    },
 });
